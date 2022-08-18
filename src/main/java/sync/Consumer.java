f0739ca79da4803fda59152a88419b7c1f3dcc1e
xzzhome <1091526909@qq.com>
2022-08-18 10:03:23 +0800

    延迟消息

--- src/main/java/sync/Consumer.java
+++ src/main/java/sync/Consumer.java
@@ -7,6 +7,8 @@ import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.common.message.MessageExt;
 
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class Consumer {
@@ -27,6 +29,7 @@ public class Consumer {
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
                 for (MessageExt messageExt : list) {
                     System.out.println(new String(messageExt.getBody(), StandardCharsets.UTF_8));
+                    System.out.println("消费时间:"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
                 }
                 return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
--- src/main/java/sync/Product.java
+++ src/main/java/sync/Product.java
@@ -5,6 +5,8 @@ import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Product {
 
@@ -24,10 +26,14 @@ public class Product {
         message.setTags("sms");//消息标签
         message.setBody(("我是消息").getBytes(StandardCharsets.UTF_8));//添加内容
 
+        //延迟级别 3，代表 10s延迟
+        message.setDelayTimeLevel(3);
+
         //执行发送
         SendResult result = producer.send(message);
         //打印结果
         System.out.println(result);
+        System.out.println("发送时间:"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
 
         producer.shutdown();
     }
